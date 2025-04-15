6ec8907fc4123eecc190b6a7284832a72c28c275
Sanjiv Kannaa Jeganathan <jsanjiv2003@gmail.com>
2025-04-16 00:58:41 +0530

    nigga

--- backend/routes/microservice_failure.py
+++ backend/routes/microservice_failure.py
@@ -3,9 +3,47 @@ from extensions import db
 import os
 from models import Resources
 from chaoslib.experiment import run_experiment
-from chaosaws.ec2.actions import restart_instances
+import subprocess
 
 microservice_failure_bp = Blueprint('microservice_failure', __name__)
+
+@microservice_failure_bp.route('/start', methods=['POST'])
+def start():
+    data = request.json
+    start_experiment = {
+        "version": "1.0.0",
+        "title": "EC2 outage",
+        "description": "Killing an EC2 instance",
+        "configuration": {
+            "aws_region": "ap-south-1"
+        },
+        "force": "True",
+        "method": [
+            {
+                "type": "action",
+                "name": "stop-instance",
+                "provider": {
+                    "type": "python",
+                    "module": "chaosaws.ec2.actions",
+                    "func": "stop_instances",
+                    "arguments": {
+                        "instance_ids": [data.get("instance_ids")]
+                        # "az": "ap-south-1a"
+                    }
+                },
+                "pauses": {
+                    "after": 10
+                }
+            }
+        ]
+    }
+    with open("start_ec2.py", "w") as f:
+        f.writelines("\nfrom chaoslib.experiment import run_experiment")
+        f.writelines("\nstart_experiment="+str(start_experiment))
+        f.writelines("\nrun_experiment(start_experiment)")
+    # return ["aws ec2 start-instances --instance-ids ", data.get("instance_ids")]
+    subprocess.run(["aws ec2 start-instances --instance-ids " + data.get("instance_ids")])
+    return make_response({"message": "done"}), 200
 
 @microservice_failure_bp.route('/stop', methods=['POST'])
 def stop():
@@ -27,7 +65,7 @@ def stop():
                     "module": "chaosaws.ec2.actions",
                     "func": "stop_instances",
                     "arguments": {
-                        "instance_ids": data.get("instance_ids")
+                        "instance_ids": [data.get("instance_ids")]
                         # "az": "ap-south-1a"
                     }
                 },
@@ -37,12 +75,12 @@ def stop():
             }
         ]
     }
-    # return stop_experiment
-    try:
-        run_experiment(stop_experiment)
-        return make_response({"message": "done"}), 200
-    except Exception as e:
-        return make_response({"error": e}), 500
+    with open("stop_ec2.py", "w") as f:
+        f.writelines("\nfrom chaoslib.experiment import run_experiment")
+        f.writelines("\nstop_experiment="+str(stop_experiment))
+        f.writelines("\nrun_experiment(stop_experiment)")
+    subprocess.run(["aws ec2 stop-instances --instance-ids ", data.get("instance_ids")])
+    return make_response({"message": "done"}), 200
 
 @microservice_failure_bp.route('/restart', methods=['POST'])
 def restart():
@@ -64,7 +102,7 @@ def restart():
                     "module": "chaosaws.ec2.actions",
                     "func": "restart_instances",
                     "arguments": {
-                        "instance_ids": data.get("instance_ids")
+                        "instance_ids": [data.get("instance_ids")]
                         # "az": "ap-south-1a"
                     }
                 },
@@ -74,9 +112,10 @@ def restart():
             }
         ]
     }
-    try:
-        run_experiment(restart_experiment)
-        return make_response({"message": "done"}), 200
-    except Exception as e:
-        return make_response({"error": e}), 500
-
+    with open("restart_ec2.py", "w") as f:
+        f.writelines("\nfrom chaoslib.experiment import run_experiment")
+        f.writelines("\nrestart_experiment="+str(restart_experiment))
+        f.writelines("\nrun_experiment(restart_experiment)")
+    subprocess.run(["aws ec2 stop-instances --instance-ids ", data.get("instance_ids")])
+    subprocess.run(["aws ec2 start-instances --instance-ids ", data.get("instance_ids")])
+    return make_response({"message": "done"}), 200
